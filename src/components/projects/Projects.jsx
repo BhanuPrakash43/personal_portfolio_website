e95e976df39f35c1a86ca591c8b9166f86c5edd8
BhanuPrakash43 <bhanu742630@gmail.com>
2025-02-23 17:46:05 +0530

    Refactor ProjectsCard component for improved layout and add project description

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,138 +1,176 @@
 import React, { useState, useEffect } from "react";
 import Title from "../layouts/Title";
 import {
-  blogImg,
-  realestateImg,
-  todoImg,
-  colorcatchingImg,
-  inventoryImg,
-  sudokuImg,
-  portfolioImg,
-  foodCart,
+	blogImg,
+	realestateImg,
+	todoImg,
+	colorcatchingImg,
+	inventoryImg,
+	sudokuImg,
+	portfolioImg,
+	androidBlog,
+	foodCart,
 } from "../../assets/index";
 import ProjectsCard from "./ProjectsCard";
 
 const Projects = () => {
-  const projects = [
-    {
-      title: "Personal Portfolio Website",
-      des: "React.js | Tailwind CSS | Framer Motion",
-      src: portfolioImg,
-      githubLink:
-        "https://github.com/BhanuPrakash43/personal_portfolio_website",
-    },
-    {
-      title: "Blog Website",
-      des: "React.js | Node.js | Express.js | MongoDB | CSS",
-      src: blogImg,
-      githubLink: "https://github.com/BhanuPrakash43/Blog-App",
-      liveLink: "https://mern-blog-app-bp.netlify.app/",
-    },
-    {
-      title: "Real Estate Website",
-      des: "React.js | CSS",
-      src: realestateImg,
-      githubLink: "https://github.com/BhanuPrakash43/Real-Estate-Website",
-      liveLink: "https://bhanu-real-estatte-website.netlify.app/",
-    },
-    {
-      title: "Todo List Website",
-      des: "React.js | CSS",
-      src: todoImg,
-      githubLink: "https://github.com/BhanuPrakash43/TODO-LIST-APP",
-      liveLink: "https://react-todo-website.netlify.app/",
-    },
-    {
-      title: "Color Catching Game",
-      des: "HTML | CSS | JavaScript",
-      src: colorcatchingImg,
-      githubLink: "https://github.com/BhanuPrakash43/Color-Catch-Game",
-    },
-    {
-      title: "Inventory Management System",
-      des: "Java | Java Swing",
-      src: inventoryImg,
-      githubLink:
-        "https://github.com/BhanuPrakash43/Inventory-Management-System",
-    },
-    {
-      title: "Mini Food Cart",
-      des: "React.js | Node.js | Express.js | MongoDB | Tailwind CSS",
-      src: foodCart,
-      githubLink: "https://github.com/BhanuPrakash43/mini-food-cart",
-      liveLink: "https://food-cart-website.netlify.app/",
-    },
-    {
-      title: "Sudoku Solver",
-      des: "Java | Java Swing",
-      src: sudokuImg,
-      githubLink: "https://github.com/BhanuPrakash43/Sudoku-Solver",
-    },
-  ];
+	const projects = [
+		{
+			title: "Blog Website",
+			des: "React.js | Node.js | Express.js | MongoDB | Tailwind CSS",
+			src: blogImg,
+			githubLink: "https://github.com/BhanuPrakash43/prime-blog",
+			liveLink: "https://prime-blog.netlify.app/",
+			projectDesc:
+				"A feature-rich blog platform with user authentication, CRUD functionality, a modern UI with Admin Dashboard.",
+			tag: "Full Stack Development",
+		},
+		{
+			title: "Personal Portfolio Website",
+			des: "React.js | Tailwind CSS | Framer Motion",
+			src: portfolioImg,
+			githubLink:
+				"https://github.com/BhanuPrakash43/personal_portfolio_website",
+			projectDesc:
+				"A responsive portfolio website showcasing my projects, skills, and experience.",
+			tag: "Frontend Development",
+		},
+		{
+			title: "Blog Fusion (Android App)",
+			des: "XML Layouts | Kotlin | Android SDK | Firebase",
+			src: androidBlog,
+			githubLink: "https://github.com/BhanuPrakash43/BlogFusion",
+			projectDesc:
+				"A feature-rich platform for creating, reading, updating, and managing blog posts with saving and liking functionalities.",
+			tag: "Android Development",
+		},
+		{
+			title: "Real Estate Website",
+			des: "React.js | CSS",
+			src: realestateImg,
+			githubLink: "https://github.com/BhanuPrakash43/Real-Estate-Website",
+			liveLink: "https://bhanu-real-estatte-website.netlify.app/",
+			projectDesc:
+				"A real estate platform for browsing, listing, and searching properties efficiently.",
+			tag: "Frontend Development",
+		},
+		{
+			title: "Todo List Website",
+			des: "React.js | CSS",
+			src: todoImg,
+			githubLink: "https://github.com/BhanuPrakash43/TODO-LIST-APP",
+			liveLink: "https://react-todo-website.netlify.app/",
+			projectDesc:
+				"A simple and intuitive task management application for organizing daily activities.",
+			tag: "Frontend Development",
+		},
+		{
+			title: "Mini Food Cart",
+			des: "React.js | Node.js | Express.js | MongoDB | Tailwind CSS",
+			src: foodCart,
+			githubLink: "https://github.com/BhanuPrakash43/mini-food-cart",
+			liveLink: "https://food-cart-website.netlify.app/",
+			projectDesc:
+				"An online food cart system where users can order and manage food items seamlessly.",
+			tag: "Full Stack Development",
+		},
+		{
+			title: "Inventory Management System",
+			des: "Java | Java Swing",
+			src: inventoryImg,
+			githubLink:
+				"https://github.com/BhanuPrakash43/Inventory-Management-System",
+			projectDesc:
+				"A Java-based inventory tracking system for managing stock efficiently.",
+			tag: "Java Application",
+		},
+		{
+			title: "Color Catching Game",
+			des: "HTML | CSS | JavaScript",
+			src: colorcatchingImg,
+			githubLink: "https://github.com/BhanuPrakash43/Color-Catch-Game",
+			projectDesc:
+				"An interactive game where players catch the correct colors before time runs out.",
+			tag: "Frontend Development",
+		},
+		{
+			title: "Sudoku Solver",
+			des: "Java | Java Swing",
+			src: sudokuImg,
+			githubLink: "https://github.com/BhanuPrakash43/Sudoku-Solver",
+			projectDesc:
+				"A Java-based Sudoku solving application that provides instant solutions to puzzles.",
+			tag: "Java Application",
+		},
+	];
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(3);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [itemsPerPage, setItemsPerPage] = useState(3);
 
-  useEffect(() => {
-    const updateItemsPerPage = () => {
-      if (window.innerWidth >= 768 && window.innerWidth < 1280) {
-        setItemsPerPage(2);
-      } else {
-        setItemsPerPage(3);
-      }
-    };
+	useEffect(() => {
+		const updateItemsPerPage = () => {
+			if (window.innerWidth >= 768 && window.innerWidth < 1280) {
+				setItemsPerPage(4);
+			} else if (window.innerWidth > 1280) {
+				setItemsPerPage(6);
+			} else {
+				setItemsPerPage(4);
+			}
+		};
 
-    window.addEventListener("resize", updateItemsPerPage);
-    updateItemsPerPage();
+		window.addEventListener("resize", updateItemsPerPage);
+		updateItemsPerPage();
 
-    return () => window.removeEventListener("resize", updateItemsPerPage);
-  }, []);
+		return () => window.removeEventListener("resize", updateItemsPerPage);
+	}, []);
 
-  const totalPages = Math.ceil(projects.length / itemsPerPage);
+	const totalPages = Math.ceil(projects.length / itemsPerPage);
 
-  const currentProjects = projects.slice(
-    (currentPage - 1) * itemsPerPage,
-    currentPage * itemsPerPage
-  );
+	const currentProjects = projects.slice(
+		(currentPage - 1) * itemsPerPage,
+		currentPage * itemsPerPage,
+	);
 
-  return (
-    <section
-      id="projects"
-      className="w-full py-14 border-b-[1px] border-b-black"
-    >
-      <div className="flex justify-center items-center text-center">
-        <Title title="My Projects" />
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
-        {currentProjects.map((project, index) => (
-          <ProjectsCard
-            key={index}
-            title={project.title}
-            des={project.des}
-            src={project.src}
-            githubLink={project.githubLink}
-            liveLink={project.liveLink}
-          />
-        ))}
-      </div>
+	return (
+		<section
+			id="projects"
+			className="w-full py-10 border-b-[1px] border-b-black"
+		>
+			<div className="flex justify-center items-center text-center">
+				<Title title="My Projects" />
+			</div>
+			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-10">
+				{currentProjects.map((project, index) => (
+					<ProjectsCard
+						key={index}
+						src={project.src}
+						title={project.title}
+						des={project.des}
+						projectDesc={project.projectDesc}
+						githubLink={project.githubLink}
+						liveLink={project.liveLink}
+						tag={project.tag}
+					/>
+				))}
+			</div>
 
-      <div className="flex justify-center mt-8">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index}
-            onClick={() => setCurrentPage(index + 1)}
-            className={`px-5 py-2 mx-3 text-lg ${
-              currentPage === index + 1
-                ? "bg-blue-500 text-white"
-                : "bg-gray-900"
-            } rounded`}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </div>
-    </section>
-  );
+			<div className="flex justify-center mt-8">
+				{Array.from({ length: totalPages }, (_, index) => (
+					<button
+						key={index}
+						onClick={() => setCurrentPage(index + 1)}
+						className={`px-5 py-2 mx-3 text-lg ${
+							currentPage === index + 1
+								? "bg-blue-500 text-white"
+								: "bg-gray-900"
+						} rounded`}
+					>
+						{index + 1}
+					</button>
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default Projects;
--- src/components/projects/ProjectsCard.jsx
+++ src/components/projects/ProjectsCard.jsx
@@ -2,42 +2,63 @@ import React from "react";
 import { BsGithub } from "react-icons/bs";
 import { FaGlobe } from "react-icons/fa";
 
-const ProjectsCard = ({ title, des, src, githubLink, liveLink }) => {
-  return (
-    <div className="w-full p-4 xl:px-6 h-auto xl:py-6 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#0b0c0d] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
-      <div className="w-full h-[90%] overflow-hidden rounded-lg">
-        <img
-          className="w-full h-50 object-cover group-hover:scale-110 duration-300 cursor-pointer"
-          src={src}
-          alt="src"
-        />
-      </div>
-      <div className="w-full mt-5 flex flex-col  gap-6">
-        <div>
-          <div className="flex items-center justify-between">
-            <h3 className="text-base uppercase text-designColor font-normal">
-              {title}
-            </h3>
-            <div className="flex gap-2">
-              <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
-                <a href={githubLink} target="_blank">
-                  <BsGithub />
-                </a>
-              </span>
-              <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
-                <a href={liveLink} target="_blank">
-                  <FaGlobe />
-                </a>
-              </span>
-            </div>
-          </div>
-          <p className="text-sm tracking-wide mt-5 hover:text-gray-100 duration-300">
-            <span className="text-md text-red-600 ">Tech Stack :</span> {des}
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+const ProjectsCard = ({
+	src,
+	title,
+	des,
+	projectDesc,
+	githubLink,
+	liveLink,
+	tag,
+}) => {
+	return (
+		<div className="w-full p-6 h-[420px] rounded-2xl shadow-lg flex flex-col bg-[#121212] border border-gray-700 group hover:shadow-xl transition-shadow duration-300">
+			<div className="relative w-full h-[200px] overflow-hidden rounded-lg">
+				{tag && (
+					<span className="absolute top-0 left-0 bg-blue-600 text-white text-xs font-semibold px-3 py-1 rounded-tl-lg rounded-br-lg shadow-lg">
+						{tag}
+					</span>
+				)}
+				<img
+					className="w-full h-full object-cover rounded-lg hover:scale-105 transition-transform duration-300 cursor-pointer"
+					src={src}
+					alt={title}
+				/>
+			</div>
+			<div className="w-full mt-3 h-[220px] flex flex-col gap-3 flex-grow">
+				<div className="flex-grow">
+					<h3 className="text-lg font-semibold text-white uppercase">
+						{title}
+					</h3>
+					<p className="text-sm text-gray-400 mt-2 line-clamp-2">
+						{projectDesc}
+					</p>
+				</div>
+				<p className="text-sm tracking-wide mt-1 text-gray-400">
+					<span className="text-md font-semibold text-blue-500">
+						Tech Stack:{" "}
+					</span>{" "}
+					{des}
+				</p>
+				<div className="flex gap-4 mt-2 h-10">
+					<a
+						href={githubLink}
+						target="_blank"
+						className="text-lg w-10 h-10 rounded-full bg-gray-800 flex justify-center items-center text-white hover:bg-blue-600 duration-300 cursor-pointer"
+					>
+						<BsGithub />
+					</a>
+					<a
+						href={liveLink}
+						target="_blank"
+						className="text-lg w-10 h-10 rounded-full bg-gray-800 flex justify-center items-center text-white hover:bg-green-600 duration-300 cursor-pointer"
+					>
+						<FaGlobe />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ProjectsCard;
