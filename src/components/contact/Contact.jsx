4877a17a6c1f98d66892880be26f4d6f58a6b6b6
BhanuPrakash43 <bhanu742630@gmail.com>
2025-02-23 17:47:10 +0530

    Refactor Contact components to enhance layout with border adjustments and improve address formatting

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -65,7 +65,7 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="w-full py-14 border-b-[1px] border-b-black"
+      className="w-full py-10 border-b-[1px] border-b-black"
     >
       <div className="flex justify-center items-center text-center">
         <Title title="Contact With Me" />
@@ -73,7 +73,7 @@ const Contact = () => {
       <div className="w-full">
         <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
           <ContactLeft />
-          <div className="w-full lgl:w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne">
+          <div className="w-full lgl:w-[60%] h-full border border-gray-700 py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne">
             <form
               ref={form}
               className="w-full flex flex-col gap-4 lgl:gap-6 py-2 lgl:py-5"
--- src/components/contact/ContactLeft.jsx
+++ src/components/contact/ContactLeft.jsx
@@ -5,7 +5,7 @@ import { contactImg } from "../../assets/index";
 
 const ContactLeft = () => {
   return (
-    <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
+    <div className="w-full lgl:w-[35%] h-full border border-gray-700 bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
       <img
         className="w-full h-44 object-cover rounded-lg mb-2"
         src={contactImg}
@@ -23,7 +23,7 @@ const ContactLeft = () => {
         <p className="text-base text-gray-400 flex items-center gap-2">
           Address:{" "}
           <span className="text-lightText">
-            Lari, Kurtha, Arwal, Bihar ( 804421 )
+            Kurtha, Arwal, Bihar ( 804421 )
           </span>
         </p>
       </div>
