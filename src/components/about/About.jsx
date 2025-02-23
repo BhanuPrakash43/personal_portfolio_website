e219d55165c31d8ca4a013376188e3af93565fc2
BhanuPrakash43 <bhanu742630@gmail.com>
2025-02-23 17:44:40 +0530

    Enhance About component with improved text formatting and content updates

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,95 +2,118 @@ import React, { useState } from "react";
 import Title from "../layouts/Title";
 
 function About() {
-  const [showFullText, setShowFullText] = useState(false);
+	const [showFullText, setShowFullText] = useState(false);
 
-  const toggleText = () => {
-    setShowFullText(!showFullText);
-  };
+	const toggleText = () => {
+		setShowFullText(!showFullText);
+	};
 
-  return (
-    <section id="about" className="w-full py-14 border-b-[1px] border-b-black">
-      <div className="flex justify-center items-center text-center">
-        <Title title={"About me"} />
-      </div>
+	return (
+		<section
+			id="about"
+			className="w-full py-10 border-b-[1px] border-b-black"
+		>
+			<div className="flex justify-center items-center text-center">
+				<Title title={"About me"} />
+			</div>
 
-      <div className="w-full h-auto bg-black bg-opacity-30 hover:bg-opacity-40 duration-300 p-8 lgl:px-10 shadow-shadowOne rounded-lg hover:text-gray-300 ">
-        <p className="text-base md:text-lg font-medium text-gray-400 hover:text-gray-300 duration-300 tracking-wide">
-          {showFullText ? (
-            <>
-              My name is Bhanu Prakash, son of Santosh Kumar and Usha Devi,
-              hailing from Bihar. I am currently pursuing a Bachelor's degree in
-              Computer Science and Engineering from Lovely Professional
-              University. I completed my secondary and senior secondary
-              schooling from Bal Vidya Niketan School in Jehanabad, Bihar. As a
-              driven and passionate software developer, I am deeply focused on
-              programming and continuously strive to acquire new skills.
-              <br />
-              <br />
-              My interest in computer science began during my school days when I
-              was eager to understand how computers work. This curiosity
-              motivated me to delve deeper into the field, exploring computer
-              applications, languages, and algorithms. This passion led me to
-              choose computer science as my field of study and career path.
-              <br />
-              <br />
-              I am proficient in JavaScript and React.js, with a strong
-              foundation in Java and database management. To hone my frontend
-              and backend skills, I have developed several projects, including a
-              blog app for reading and writing blogs, a portfolio website, a
-              real estate website, a to-do list app and many more. My aspiration
-              is to secure a position in a reputable company and forge a
-              successful career in software engineering.
-              <br />
-              <br />I am currently seeking internships and full-time
-              opportunities to launch my career and gain practical experience.
-              My enthusiasm for programming is matched by my disciplined
-              approach and commitment to continuous learning, enabling me to
-              excel in fast-paced environments. I thrive in teamwork, delivering
-              high-quality results and meeting deadlines. Apart from my academic
-              pursuits, I have a keen interest in fitness, cricket, and
-              volleyball, firmly believing that discipline is paramount in
-              achieving our goals and establishing a prosperous career. I am
-              eager to contribute to innovative projects and grow
-              professionally.
-            </>
-          ) : (
-            <>
-              My name is Bhanu Prakash, son of Santosh Kumar and Usha Devi,
-              hailing from Bihar. I am currently pursuing a Bachelor's degree in
-              Computer Science and Engineering from Lovely Professional
-              University. I completed my secondary and senior secondary
-              schooling from Bal Vidya Niketan School in Jehanabad, Bihar. As a
-              driven and passionate software developer, I am deeply focused on
-              programming and continuously strive to acquire new skills.
-              <br />
-              <br />
-              My interest in computer science began during my school days when I
-              was eager to understand how computers work. This curiosity
-              motivated me to delve deeper into the field, exploring computer
-              applications, languages, and algorithms. This passion led me to
-              choose computer science as my field of study and career path.
-              <br />
-              <button
-                onClick={toggleText}
-                className="text-blue-500 hover:underline"
-              >
-                Read more
-              </button>
-            </>
-          )}
-        </p>
-        {showFullText && (
-          <button
-            onClick={toggleText}
-            className="mt-4 text-blue-500 hover:underline"
-          >
-            Show less
-          </button>
-        )}
-      </div>
-    </section>
-  );
+			<div className="w-full h-auto bg-[#0b0b0b] border border-gray-700 bg-opacity-30 hover:bg-opacity-40 duration-300 p-8 lgl:px-10 shadow-shadowOne rounded-lg hover:text-gray-300 ">
+				<p className="text-base md:text-lg font-medium text-justify text-gray-400 hover:text-gray-300 duration-300 tracking-wide">
+					{showFullText ? (
+						<>
+							My name is Bhanu Prakash, hailing from Bihar. I am
+							currently pursuing a Bachelor’s degree in Computer
+							Science and Engineering from Lovely Professional
+							University. I completed my secondary and senior
+							secondary education at Bal Vidya Niketan School in
+							Jehanabad, Bihar. As a passionate and driven
+							software developer, I am deeply focused on
+							programming and continuously strive to expand my
+							skill set.
+							<br />
+							<br />
+							My interest in computer science began during my
+							school days when I became curious about how
+							computers work. This curiosity led me to explore
+							computer applications, programming languages, and
+							algorithms, ultimately shaping my decision to pursue
+							computer science as both my field of study and
+							career path.
+							<br />
+							<br />
+							I am proficient in JavaScript, React.js, Node.js,
+							Express.js, and MongoDB, with a strong foundation in
+							Java and database management. To enhance my frontend
+							and backend skills, I have developed several
+							projects, including: A blog platform with user
+							authentication, commenting, and an admin dashboard,
+							A personal portfolio website to showcase my work and
+							skills, A to-do list website for efficient task
+							management and many more. My aspiration is to secure
+							a position in a reputable company where I can
+							contribute effectively and build a successful career
+							in software engineering.
+							<br />
+							<br />
+							I am currently seeking internships and full-time
+							opportunities to gain hands-on experience and apply
+							my skills in real-world projects. My enthusiasm for
+							programming is complemented by my disciplined
+							approach and commitment to continuous learning,
+							enabling me to excel in fast-paced environments. I
+							thrive in teamwork, consistently delivering
+							high-quality results while meeting deadlines.
+							<br />
+							<br />
+							Beyond academics and programming, I have a strong
+							interest in fitness, cricket, and volleyball, as I
+							believe that discipline and consistency are crucial
+							in achieving both professional and personal success.
+							I am eager to contribute to innovative projects and
+							grow professionally in the field of software
+							development.
+						</>
+					) : (
+						<>
+							My name is Bhanu Prakash, hailing from Bihar. I am
+							currently pursuing a Bachelor’s degree in Computer
+							Science and Engineering from Lovely Professional
+							University. I completed my secondary and senior
+							secondary education at Bal Vidya Niketan School in
+							Jehanabad, Bihar. As a passionate and driven
+							software developer, I am deeply focused on
+							programming and continuously strive to expand my
+							skill set.
+							<br />
+							<br />
+							My interest in computer science began during my
+							school days when I became curious about how
+							computers work. This curiosity led me to explore
+							computer applications, programming languages, and
+							algorithms, ultimately shaping my decision to pursue
+							computer science as both my field of study and
+							career path.
+							<br />
+							<button
+								onClick={toggleText}
+								className="text-blue-500 hover:underline"
+							>
+								Read more
+							</button>
+						</>
+					)}
+				</p>
+				{showFullText && (
+					<button
+						onClick={toggleText}
+						className="mt-4 text-blue-500 hover:underline"
+					>
+						Show less
+					</button>
+				)}
+			</div>
+		</section>
+	);
 }
 
 export default About;
